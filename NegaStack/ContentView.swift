2638dc2fed89b88c3fa29b20edac90eb7b1f4ca2
Kaisei <tidakakaka12@gmail.com>
2025-11-20 01:28:02 +0900

    Initial Commit

---
+++ NegaStack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NegaStack
+//
+//  Created by 千田海生 on 2025/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NegaStack/NegaStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NegaStackApp.swift
+//  NegaStack
+//
+//  Created by 千田海生 on 2025/11/20.
+//
+
+import SwiftUI
+
+@main
+struct NegaStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
